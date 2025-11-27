47fb35e23f9a58058156c4cc560c1c5e64904cf6
verchy1 <verchyfresneloba@gmail.com>
2025-11-27 14:16:21 +0100

    Update CORS configuration to allow requests only from production URL

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middlewares
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['https://subscmanager.netlify.app'],
   credentials: true
 };
 app.use(cors(corsOptions));
